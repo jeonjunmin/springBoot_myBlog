1e9b6db744aca4661b66ec53a305ae02cf70b797
jeonjunmin <azeros314@naver.com>
2021-06-27 21:07:30 +0900

    no message

--- src/main/java/com/example/myBlog/MyBlogApplication.java
+++ src/main/java/com/example/myBlog/MyBlogApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class MyBlogApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MyBlogApplication.class, args);
+
 	}
 
 }
